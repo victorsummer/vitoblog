b7d6901d31426e9a395cd869e24404dd959b44a9
Vito Ao <victorsummer@gmail.com>
2017-11-10 18:37:15 +0800

    Adjust code font size

--- src/views/post.vue
+++ src/views/post.vue
@@ -58,7 +58,7 @@ export default {
   div.post div.content pre {
     background-color: #eee;
     overflow-x: scroll;
-    padding: 0.1rem 0.2rem;
+    padding: 0.1rem 0.1rem;
     font-size: 14px;
   }
   div.post div.content blockquote > p{
